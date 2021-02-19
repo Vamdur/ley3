25c858192a8666381b7c408f8c6bea7b27f5753e
Ambrosio Duarte <78082038+Vamdur@users.noreply.github.com>
2021-02-19 11:06:15 -0400

    Add files via upload

---
+++ blanco.php
@@ -0,0 +1,7 @@
+<?php
+  echo "<script>
+    var myWindow = window.open('', '_self');
+    myWindow.document.write('<center><h1>Muchas Gracias...<h2></h2></h1><img  src=\'sysConsultaLey/img/covid19.png\'> </center>');
+  </script>";
+  die(".");
+?>
